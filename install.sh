a48a9e7de99d634e02f6b0cf7de333bcc5ed34b1
João Gabriel Josephik <joao.gabrielaaj@gmail.com>
2025-04-24 13:59:40 -0300

    Adds support to zsh.

--- install.sh
+++ install.sh
@@ -2,5 +2,8 @@
 if [[ -z "$(cat ~/.bashrc | grep "PYTHONPATH=\$PYTHONPATH:~/.pbin")" ]]; then
     printf "\nPYTHONPATH=\$PYTHONPATH:~/.pbin" >> ~/.bashrc
 fi
+if [[ -z "$(cat ~/.zshrc | grep "PYTHONPATH=\$PYTHONPATH:~/.pbin")" ]]; then
+    printf "\nPYTHONPATH=\$PYTHONPATH:~/.pbin" >> ~/.zshrc
+fi
 mkdir -p ~/.pbin/jjlib
 cp -r ./src ~/.pbin/jjlib
